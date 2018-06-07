844fac6af81a3a01e01ad734419566506495d723
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2018-06-07 22:16:01 +0000

    add Semestres e turmas no navbar, restricao de acesso semestres #36

--- app/controllers/semesters_controller.rb
+++ app/controllers/semesters_controller.rb
@@ -1,5 +1,6 @@
 class SemestersController < ApplicationController
   before_action :set_semester, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /semesters
   # GET /semesters.json
