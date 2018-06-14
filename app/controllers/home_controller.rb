daa88ec38a8a73d6a5d6dfb62468e7b234e97d0b
José Ricardo <zevatron@zevatron.com.br>
2018-06-14 10:35:09 -0300

    Revert "busca por nome do usuário e disciplina e botão para ir direto para tela de eventos"

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,12 @@
 class HomeController < ApplicationController
   def index
     @teams = Team.all
-    @chave = " ";
-     if params[:search]
-      @chave = params[:search]
-    end
 
-    # if params[:search]
-    #   @people = Person.where("nome LIKE ?", "%"+params[:search]+"%")
-    # else
-    #   @people = nil
-    # end
+    if params[:search]
+      @people = Person.where("nome LIKE ?", "%"+params[:search]+"%")
+    else
+      @people = nil
+    end
   end
 
 end
